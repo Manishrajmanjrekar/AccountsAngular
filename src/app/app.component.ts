fe635cb33eb991f5ee0ae2a00fc9f72f501915af
Manish <manishmanjrekar@outlook.com>
2019-04-12 14:31:43 +0530

    first cut branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
-
+//import { Component } from '@angular/core';
+import { Component, 
+  OnInit, 
+  ViewChild, 
+  ComponentFactoryResolver,
+  ViewContainerRef } from '@angular/core';
+import { AppSalesComponent } from './app-sales/app-sales.component';
+import { FormBuilder, FormGroup,FormArray, Validators } from '@angular/forms';
+  //import { AppSalesComponent } from '../app/app-sales/AppSalesComponent';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +14,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-Accounts';
+  items: any;
+  @ViewChild('parent', { read: ViewContainerRef }) container: ViewContainerRef;
+
+   constructor(private _cfr: ComponentFactoryResolver) { }
+   ngOnInit(){ }
+
+  addComponent(){    
+      var comp = this._cfr.resolveComponentFactory(AppSalesComponent);
+      var appSalesComponent = this.container.createComponent(comp);
+      appSalesComponent.instance._ref = appSalesComponent;
+  }
+  OnSubmit($event: Event) {
+   // $event.preventDefault();
+   console.log('save form called')
+    this.items =event;
+    console.log(this.items)
+   
+}
+}
+export class SalesInfo{
+  custInfo:Array<any>;
+  vendorInfo:Array<any>;
+  Price:number;
+  Quantity:number;
+  Total:number;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ViewChild, 
+  ComponentFactoryResolver,
+  ViewContainerRef } from '@angular/core';
+ // import { AppSalesComponent } from './app-sales/AppSalesComponent';
 
+import { FormsModule ,ReactiveFormsModule }   from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { AppSalesComponent } from './app-sales/app-sales.component';
+import { CustomerInfoComponent } from './customer-info/customer-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    ContactFormComponent,
+    AppSalesComponent,
+    CustomerInfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents : [AppSalesComponent]
 })
 export class AppModule { }
